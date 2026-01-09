e6f28a994a840862f0f86f889e7ca4e888ee3aa9
Aymaan Khan <aymaankhan@Aymaans-MacBook-Pro-4.local>
2026-01-09 16:23:57 -0700

    Complete Lab 01

---
+++ app/src/main/java/com/example/petshop/SadMood.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class SadMood extends Mood {
+    public SadMood() {
+        super();
+    }
+
+    public SadMood(Date date) {
+        super(date);
+    }
+
+    @Override
+    public String moodIs() {
+        return "sad";
+    }
+}
